042106c3ba36a602d49f6241d4506cbdf6f2a0ba
José Luis <joseluis@legux.com>
2018-05-31 17:15:59 -0500

    tarea 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Madre teresa de calcuta, es la mujer más buena que conozco';
 }
